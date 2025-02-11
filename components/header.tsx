1aea3c7d04b6be5cce1d907b72d8519a30768bba
JesusWor <programjesuse@gmail.com>
2025-02-11 17:11:31 -0600

    Actualizacion 4

--- components/header.tsx
+++ components/header.tsx
@@ -1,26 +1,30 @@
-import Link from "next/link"
-import MotionTransition from "./transition-component"
-import { socialNetworks } from "@/data"
+"use client"
+
+import { socialNetworks } from "@/data";
+import Link from "next/link";
+import MotionTransition from "./transition-component";
 
 const Header = () => {
     return (
-        <MotionTransition position="bottom" className="absolute z-40 inline-block w-full top-5 md:top-10 px-20">
+        <MotionTransition position="bottom" className="absolute z-40 inline-block w-full top-5 md:top-10">
             <header>
-                <div className="container justify-between max-w-6xl mx-auto">
-                    <Link href="/">
-                        <h1 className="my-3 text-4xl font-bold text-center md:text-left">
-                            Jesus Eduardo{' '}
+                <div className="container justify-between max-w-6xl mx-auto md:flex">
+                    <Link href='/'>
+                        <h1 className="my-3 text-2xl font-bold text-center md:text-left">
+                            Jesús Eduardo{" "}
                             <span className="text-secondary">
                                 Escobar Meza
                             </span>
-                        </h1>   
+                        </h1>
                     </Link>
                     <div className="flex items-center justify-center gap-7">
-                        {socialNetworks.map(({logo, src, id}) => (
-                            <Link key={id}
-                            href={src}
-                            target="_blank"
-                            className="transition-all duration-300 hover:text-secondary">
+                        {socialNetworks.map(({ logo, src, id }) => (
+                            <Link
+                                key={id}
+                                href={src}
+                                target="_blank"
+                                className="transition-all duration-300 hover:text-secondary"
+                            >
                                 {logo}
                             </Link>
                         ))}
@@ -28,7 +32,7 @@ const Header = () => {
                 </div>
             </header>
         </MotionTransition>
-    )
+    );
 }
 
-export default Header
+export default Header;
